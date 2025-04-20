4dca93172cfa6f17a8372eb62153879f70e22259
Rashnerd <akhigbek6@gmail.com>
2025-04-20 01:54:14 +0100

    Removed .env from public repo

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.conf import settings
+
+from api import models
 
 # Register your models here.
+admin.site.register(models.CustomUser)
+admin.site.register(models.UserProfile)
+admin.site.register(models.FriendRequest)
+admin.site.register(models.FriendsList)
